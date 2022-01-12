2884d9f22705876f70a686f593a08768ba29abff
Julio Briones <Julio_Briones@epam.com>
2022-01-12 13:38:43 -0600

    Initial commit, added basic app, templates and test-database connectivity; WIP.

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def show_projects():
+    """List all projects."""
+    return render_template("index.html")
+
+
+@app.route("/project/<project_id>")
+def show_tasks(project_id):
+    """List all tasks."""
+    return render_template("project-tasks.html", project_id=project_id)
+
+
+@app.route("/add/project", methods=["POST"])
+def add_project():
+    """Add a new project."""
+    return "Project added successfully"
+
+
+@app.route("/add/task/<project_id>", methods=["POST"])
+def add_task(project_id):
+    """Add a new task."""
+    return "Task added successfully"
+
+
+app.run(debug=True, host="127.0.0.1", port=3000)
